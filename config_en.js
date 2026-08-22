342f8b5154c48d4a8c9e05aea69781cc17894ec0
namoamitabhapureland <huangsir945@gmail.com>
2026-08-22 13:45:00 +0800

    Add files via upload

---
+++ config_en.js
@@ -0,0 +1,26 @@
+// config_en.js - Amitabha Pure Land System (English) Festival & Dew Quotes
+
+const festivalMap = {
+    "01-05": "Amitabha Buddha's Birthday", "12-25": "Amitabha Buddha's Birthday",
+    "04-06": "Avalokitesvara's Birthday", "08-01": "Avalokitesvara's Enlightenment", "10-28": "Avalokitesvara's Renunciation",
+    "09-10": "Ksitigarbha Bodhisattva's Birthday", "05-24": "Shakyamuni Buddha's Birthday",
+    "03-26": "Shakyamuni Buddha's Renunciation", "04-02": "Shakyamuni Buddha's Nirvana",
+    "01-19": "Lunar 1st", "02-02": "Lunar 15th", "02-17": "Lunar 1st", "03-03": "Lunar 15th", "03-19": "Lunar 1st",
+    "04-17": "Lunar 1st", "05-01": "Lunar 15th", "05-17": "Lunar 1st", "05-31": "Lunar 15th", "06-15": "Lunar 1st",
+    "06-29": "Lunar 15th", "07-14": "Lunar 1st", "07-28": "Lunar 15th", "08-13": "Lunar 1st", "08-27": "Lunar 15th",
+    "09-11": "Lunar 1st", "09-25": "Lunar 15th", "10-10": "Lunar 1st", "10-24": "Lunar 15th", "11-09": "Lunar 1st",
+    "11-23": "Lunar 15th", "12-08": "Lunar 1st", "12-22": "Lunar 15th"
+};
+
+
+const dewQuotes = [
+    "One recitation of 'Namo Buddha', and the path to Buddhahood is sealed.", 
+    "The mind is like a master painter, able to paint all worlds.", 
+    "Arouse the mind without resting it upon anything.",
+    "All appearances are illusory.", 
+    "All conditioned phenomena are like dreams, illusions, bubbles, and shadows.", 
+    "Mindfulness of the Buddha extinguishes past karma.",
+    "Faith is the source of the path and the mother of merit.", 
+    "Gather the six senses, maintain continuous pure mindfulness.",
+    "One thought corresponds, one thought is Buddha."
+];
